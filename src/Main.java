b8496b6e44f549df8e5fadebc96698178f9be05d
Prosikova <lvenok182@mail.ru>
2022-09-12 21:11:21 +0300

    Задание 1. Домашнего задания 3_4 (переменные 2 часть)

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+int cars = 210;
+        System.out.println("Значение переменной " + "cars " + "с типом " + "int " + "равно " + cars);
+        byte roomsHouse = 4;
+        System.out.println("Значение переменной " + "roomsHouse " + "с типом " + "byte " + "равно " + roomsHouse);
+        short money = 32765;
+        System.out.println("Значение переменной " + "money " + "с типом " + "short " + "равно " + money);
+        long populationCity = 5345487L;
+        System.out.println("Значение переменной " + "populationCity " + "с типом " + "long " + "равно " + populationCity);
+        double change = 273.74;
+        System.out.println("Значение переменной " + "change " + "с типом " + "double " + "равно " + change);
+        float apple = 0.2f;
+        System.out.println("Значение переменной " + "apple " + "с типом " + "float " + "равно " + apple);
+    }
+}
